36a6692d714be080fd40f760dfee51b39edee7a9
Lucas Aquiles <lucasaquiles@gmail.com>
2020-03-06 16:19:59 -0300

    config do redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { recorderButton } from './actions';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" style={{ padding: '10px' }}>
-          <button>
-              recorder
-          </button>
-    </div>
-  );
+class App extends React.Component {
+
+  inputChange = event => {
+
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  render() {
+    
+    const {
+      recorderButton,
+      value
+    } = this.props;
+
+    return (
+      <div className="App" style={{ padding: '10px' }}>
+            
+            <button onClick={() => recorderButton(value)}>
+               Recorder {value}
+            </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ recorderButton }, dispatch);
+
+const mapStateToProps = store => ({
+  value: store.recorderState.value
+});
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
