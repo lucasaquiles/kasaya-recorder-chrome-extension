60478bbec78511eb54d7dc123994fc87e2068fc0
Lucas Aquiles <lucasaquiles@gmail.com>
2020-03-06 15:07:07 -0300

    reconfiguração do projeto

---
+++ build/precache-manifest.fa42e3f273436b56b2a61bd879c0eae1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd894137b7f5333615b86e4d5333eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8cec6e422f6eac62a00",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c10099c699ec889dec1e",
+    "url": "/static/js/2.cee420fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cee420fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cec6e422f6eac62a00",
+    "url": "/static/js/main.719f1d52.chunk.js"
+  },
+  {
+    "revision": "a7ef920fbfc3123207b6",
+    "url": "/static/js/runtime-main.da8ba309.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ padding: '10px' }}>
+          <button>
+              recorder
+          </button>
+    </div>
+  );
+}
+
+export default App;
