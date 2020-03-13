3564281f4a8325b4b04a8e1f3f0d384e8d223778
Lucas Aquiles <lucasaquiles@gmail.com>
2020-03-13 12:04:01 -0300

    stop script handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 /*global chrome*/
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { startRecord,stopRecord } from './actions';
+import { startRecord, stopRecord } from './actions';
 import './App.css';
 
-const mapDispatchToProps = dispatch => 
+const mapDispatchToProps = dispatch =>
   bindActionCreators({ startRecord, stopRecord }, dispatch);
 
 const mapStateToProps = store => ({
@@ -20,93 +20,105 @@ class App extends Component {
     status: false
   }
 
+  stopRecording = () => {
 
-  startRecording = () => {
-    
-  const code = `
-    (function getContent(){
+    const code = `
+      (function updateStorage(){
+        
+        const requestedUrl = window.location.href;
 
-      const requestedUrl = window.location.href;
-      var element;
-      document.addEventListener("click", function(event) {  
-          
-          event.preventDefault();
-          element = event.target;
-
-          var obj = {
-            tag: element.tagName,
-            value: element.textContent
-          };
-          console.log(obj);
-
-          if(!localStorage.getItem(requestedUrl)) {
-            let item = new Array(obj);
-            localStorage.setItem(requestedUrl, JSON.stringify(item))
-          } else {
-
-            var local = localStorage.getItem(requestedUrl);
-            let array = JSON.parse(local)
-            array.push(obj);
-            
-            localStorage.setItem(requestedUrl, JSON.stringify(array));
-          }
-
-          localStorage.getItem(requestedUrl);  
-      }, false);
-      
-     
-      const el  = {
-        url: requestedUrl,
-        json: localStorage.getItem(requestedUrl)
-      };
-
-      return el
-    })();
+        return localStorage.getItem(requestedUrl);
+      });
     `;
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    this.props.stopRecord()
+  }
 
-      var port = chrome.runtime.connect({name: tabs[0].id.toString()});
+  startRecording = () => {
+
+    const code = `
+      (function getContent(){
+
+        const requestedUrl = window.location.href;
+        var element;
+        document.addEventListener("click", function(event) {  
+            
+            event.preventDefault();
+            element = event.target;
+
+            var obj = {
+              tag: element.tagName,
+              value: element.textContent
+            };
+            console.log(obj);
+
+            if(!localStorage.getItem(requestedUrl)) {
+              let item = new Array(obj);
+              localStorage.setItem(requestedUrl, JSON.stringify(item))
+            } else {
+
+              var local = localStorage.getItem(requestedUrl);
+              let array = JSON.parse(local)
+              array.push(obj);
+              
+              localStorage.setItem(requestedUrl, JSON.stringify(array));
+            }
+
+            localStorage.getItem(requestedUrl);  
+        }, false);
+        
+        const el  = {
+          url: requestedUrl,
+          json: localStorage.getItem(requestedUrl)
+        };
+
+        return el
+      })();
+      `;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+      var port = chrome.runtime.connect({ name: tabs[0].id.toString() });
       // port.postMessage({tab: tabs[0].title});
 
-      var obj = {"tab": tabs[0].title};
+      var obj = { "tab": tabs[0].title };
       localStorage.setItem(tabs[0].id, obj);
 
-      chrome.tabs.executeScript(tabs[0].id , { code }, function(result) {
-    
+      chrome.tabs.executeScript(tabs[0].id, { code }, function (result) {
+
         console.log("ok", result);
 
-        port.postMessage({tab: result});
-      }); 
-      
-      port.onMessage.addListener(function(msg) {
-        
+        port.postMessage({ tab: result });
+      });
+
+      port.onMessage.addListener(function (msg) {
+
         console.log("no app: ", msg);
 
-        port.postMessage({answer: "Ok"});
+        port.postMessage({ answer: "Ok" });
       });
-      
+
     });
 
     this.props.startRecord()
   }
-  
+
   render() {
 
-    if(this.props.status) {
+    if (this.props.status) {
       return (
         <div className="App" style={{ padding: '10px' }}>
-             <button onClick={() => stopRecord()}>
-                Pause
+          <button onClick={() => stopRecord()}>
+            Pause
               </button>
         </div>
       );
-    }else{
+    } else {
       return (
         <div className="App" style={{ padding: '10px' }}>
           <button onClick={() => this.startRecording()}>Record</button>
         </div>
-      ) 
+      )
     }
   }
 }
